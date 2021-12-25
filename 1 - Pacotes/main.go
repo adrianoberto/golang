1c45d1a0170529320847b92f8762ed23bbad5a1b
Adriano Berto <adriano.berto@outlook.com>
2021-12-25 01:34:56 -0300

    Pacotes Externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxilliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxilliar.Escrever()
+
+	erro := checkmail.ValidateFormat("meutestegmailcom")
+	fmt.Println(erro)
 }
