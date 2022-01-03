893d2ca6fcf900b2e589d55a44e53fe4a7414a21
Adriano Berto <adriano.berto@outlook.com>
2022-01-03 16:44:05 -0300

    Canais com buffer

---
+++ 18 - Concorrencia/4 - canais com buffer/canais-com-buffer.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	canal := make(chan string, 2)
+	canal <- "Ola Mundo"
+	canal <- "Ola Mundo 2"
+
+	mensagem := <-canal
+	mensagem2 := <-canal
+
+	fmt.Println(mensagem)
+	fmt.Println(mensagem2)
+}
